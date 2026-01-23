99fc3ed23cadabc79ee3bddd168733adca3cf18b
Class-dashboard-site <11cmarygiri@gmail.com>
2026-01-23 14:13:19 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,44 @@
-const CACHE_NAME = "class11c-dashboard-v1";
+const CACHE_NAME = "class11c-offline-v2";
 
 const ASSETS = [
   "./",
   "./Timed_Themes.html",
-  "./manifest.json"
+  "./manifest.json",
+
+  // backgrounds
+  "./bg1.png",
+  "./bg2.png",
+  "./bg3.png",
+  "./bg4.png",
+  "./bg5.png",
+  "./bg6.png",
+  "./bg7.png",
+  "./bg8.png",
+  "./bg9.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
   );
 });
